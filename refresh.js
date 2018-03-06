4d0b7d3e6a3ec104266a9c498a37360fa5fa279b
Jonas Funcke <funckejonas@gmail.com>
2018-03-06 07:13:36 +0100

    Added greeting depending on time

--- refresh.js
+++ refresh.js
@@ -4,11 +4,24 @@ function refresh(){
     var currentdate = new Date();
     document.getElementById("time").innerHTML = currentdate.getHours() + ':' + currentdate.getMinutes() + ':' + currentdate.getSeconds();
     if((currentdate.getHours() >= 19 && currentdate.getHours() < 24)  || (currentdate.getHours() >= 0 && currentdate.getHours() < 6))
-        document.body.style.background = 'url("night-sky-16781.png")';
-    if(currentdate.getHours() >= 6 && currentdate.getHours() <= 7)
+    {
+       document.body.style.background = 'url("night-sky-16781.png")';
+       document.getElementById('city').innerHTML = 'Good Evening'; 
+    }
+        
+    if(currentdate.getHours() >= 6 && currentdate.getHours() <= 7){
         document.body.style.background = 'url("morning-mist-wallpapers-198.jpg")';
-    if(currentdate.getHours() >=8 && currentdate.getHours() <= 16)
+        document.getElementById('city').innerHTML = 'Good Morning';
+    }
+        
+    if(currentdate.getHours() >=8 && currentdate.getHours() <= 16){
         document.body.style.background = 'url("jkbOTFK.jpg")';
-    if(currentdate.getHours() >=17 && currentdate.getHours() < 19)
+        document.getElementById('city').innerHTML = 'Hey! How are your?';
+    }
+        
+    if(currentdate.getHours() >=17 && currentdate.getHours() < 19) {
         document.body.style.background = 'url("sunset-hd-wallpapers-8.jpg")';
+        document.getElementById('city').innerHTML = 'Goot Evening!';
+    }
+        
 }
